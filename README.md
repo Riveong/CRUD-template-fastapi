91ce68308b79d9bdb19b9fa033f8120a5b7b7536
Ong Riverine <53504975+Riveong@users.noreply.github.com>
2023-07-28 15:29:13 +0000

    yawn

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# CRUD-template-fastapi
+# CRUD-template-fastapi  
+<b>welcome</b>  
+idk what to say, the title already explained everything...  
+all I wanna say is that I'm too lazy to start over again. So I would rather make this dummy template thingy.  
+![asdasd](https://media.tenor.com/s9d8aqFYhgQAAAAC/yuru-camp.gif))
---
+++ main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+
+data = {
+    1:{"something":"a"},
+    2:{"something":"b"},
+    3:{"something":"c"},
+}
+
+
+
+app = FastAPI()
+
+@app.get("/")
+def getItems():
+    return data
+
+
+@app.get("/{id}")
+def getItem(id:int):
+    return data[id]
+
+@app.post("/")
+def addItem(something):
+    newId = len(data.keys()) + 1
+    data[newId] = {"something":something}
+    return data
+
+
+@app.put("/{id}")
+def updateItem(id:int, somethinga):
+    data[id]['something'] = somethinga
+    return data
+
+
+@app.delete("/{id}")
+def deleteItem(id:int):
+    del data[id]
+    return data
