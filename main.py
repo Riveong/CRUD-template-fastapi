1b647b8dea9d47bdb0640baa7563fd38eccd3489
Ong Riverine <53504975+Riveong@users.noreply.github.com>
2023-08-07 01:13:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,13 +17,6 @@ todos = [
 
 data = "what"
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
 
 
 
